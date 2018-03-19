496a68f5b99ca0e706b133bb2209c286368e3df3
Thomas <thomaschou861205@gmail.com>
2018-03-19 14:15:19 +0800

    complete a single statement

--- prefix_boolean_expression.cpp
+++ prefix_boolean_expression.cpp
@@ -2,34 +2,108 @@
 
 using namespace std;
 
-int isBool(char ch){
-	if (ch == '1' || ch == '0')
-		return 1;
-	else
-		return 0;
+typedef struct op
+{
+	char ch;
+	struct op *next;
+} OP;
+
+OP *APPEND(OP *head, char ch){
+	static OP *rear = NULL;
+	OP *node;
+
+	if (rear == NULL){
+		head->ch = ch;
+		rear = head;
+	}
+	else{
+		node = new OP;
+		node->ch = ch;
+		node->next = NULL;
+		rear->next = node;
+		rear = node;
+	}
+
+	return head;
 }
 
-int evac(char * input){
+void PRINT(OP *head){
+	OP *i;
+	for (i = head; i!=NULL; i=i->next){
+		cout << i->ch;
+	}
+	cout<<endl;
+}
 
-	if (*input == '|'){
-		if (isBool( *(input+1)) && isBool( *(input+2)))
-			return evac(input+1) || evac(input+2);
-		else
-			return evac(input+1);
+int isBool(char ch){
+	if (ch == '1' || ch == '0'){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int getBool(char ch){
+	return ch == '1'? 1:0;
+}
+
+char toBool(bool n){
+	return n? '1':'0';
+}
+
+OP *evac(OP *head){
+	OP *i;
+	OP *temp = NULL;
+
+	for (i = head; i!=NULL; ){
+		if (i->ch == '|'){
+			if (isBool((i->next)->ch) && isBool(((i->next)->next)->ch)){
+				i->ch = toBool(getBool((i->next)->ch) || getBool(((i->next)->next)->ch));
+				temp = ((i->next)->next)->next;
+				i->next = temp;
+				// delete i->next;
+				// delete (i->next)->next;
+			}
+			else{
+				i = i->next;
+			}
+		}
+		else if (i->ch == '&'){
+			if (isBool((i->next)->ch) && isBool(((i->next)->next)->ch)){
+				i->ch = toBool(getBool((i->next)->ch) && getBool(((i->next)->next)->ch));
+				temp = ((i->next)->next)->next;
+				i->next = temp;
+				// delete i->next;
+				// delete (i->next)->next;
+			}
+			else{
+				i = i->next;
+			}
+		}
+		else{
+			i = i->next;
+		}
 	}
 
-	else if (*input == '&'){
-		if (isBool( *(input+1)) && isBool( *(input+2)))
-			return evac(input+1) && evac(input+2);
-	}
-	else if(isBool(*input)){
-		return ch=='1'?1:0;
-	}
-	else return -1;
+	return head;
 }
 
 int main(){
-	char input[] = "|&10|00";
-	cout << evac(input);
+	char ch;
+	OP *head;
+
+	head = new OP;
+	head->next = NULL;
+
+	while (cin >> ch){
+		APPEND(head, ch);
+	}
+	PRINT(head);
+	while(!isBool(head->ch)){
+		evac(head);
+		PRINT(head);
+	}
+	cout << "ans: " << head->ch;
 	return 0;
 }
